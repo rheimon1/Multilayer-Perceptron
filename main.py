77a0de8d1a4aa113a4ee9ace35d42a12829d79c1
Wallace <wallacerheimon@gmail.com>
2022-07-05 14:39:55 -0300

    chore: add code documentation

--- main.py
+++ main.py
@@ -4,45 +4,86 @@ from mlp import Mlp
 import numpy as np
 
 class Main:
+  """
+  Classe que vai adaptar e tratar os dados para posteriormente ser encaminhados para a classe `Mlp`.
+  Essa classe é muito importante para o uso da classe `Mlp`, pois ela organiza e encaminha as informações 
+  no formato que a classe espera.
+  """
+
   def __init__(
     self, 
     csv_provider: CsvProvider, 
-    activation_function: SigmoidActivation, 
+    activation_function, 
     input_length, 
     output_length, 
     hidden_length,
     learning_rate,
     threshold
   ):
+    """
+    Parameters
+    ----------
+    csv_provider : Instãncia de classe que possibilita manipular arquivos csv
+    activation_function : Instãncia de classe que implementa função de ativação, bem como sua derivada
+    input_length : Define quantos elementos terão na camada de entrada
+    output_length : Define quantos elementos terão na camada de saída
+    hidden_length : Define quantos elementos terão na camada escondida
+    learning_rate : Taxa de aprendizado
+    threshold : Limite de erro
+    """
+
     self.csv_provider = csv_provider
 
     self.input_length = input_length
     self.output_length = output_length
     self.hidden_length = hidden_length
 
-    layers = {
-      'input_length': input_length,
-      'output_length': output_length,
-      'hidden_length': hidden_length
-    }
-
     self.mlp = Mlp(
-      layers=layers,
+      layers={
+        'input_length': input_length,
+        'output_length': output_length,
+        'hidden_length': hidden_length
+      },
       activation_function=activation_function,
       learning_rate=learning_rate,
       threshold=threshold
     )
 
   def convert_str_values_to_int_in_matrix(self, matrix):
+    """Converte os valores de uma matriz do tipo `String` para `Integer`. 
+
+    Parameters
+    ----------
+    matrix : Matriz com os valores a ser convertidos
+    """
+
     return [[int(i) for i in j] for j in matrix]
 
   def convert_negative_values_to_zero_in_matrix(self, matrix):
+    """Converte os valores de uma matriz que são negativos (-1) para 0.
+
+    Parameters
+    ----------
+    matrix : Matriz com os valores a ser convertidos
+    """
+
     for i in range(len(matrix)):
       for j in range(len(matrix[i])):
         if (matrix[i][j] < 0):
           matrix[i][j] = 0
 
   def prepare_data(self, dataset):
+    """Função responsável por converter os dados de um dataset para o formato esperado pela Classe `Mlp`.
+    Isso envolve separar dados de entrada de dados que são os rótulos. Converter o tipo dos dados de
+    String para Integer a fim de tornar possível os cálculos. Converter os valores negativos para zero e
+    por fim definir o formato das matrizes com auxílio de uma biblioteca chamada `numpy` que vai facilitar
+    as operações com matrizes e arrays.
+
+    Parameters
+    ----------
+    dataset : Dados obtidos de alguma fonte externa que serão analisados, tratados e organizados. 
+    """
+
     input_data = [ [] for _ in range(len(dataset)) ]
     label_data = [ [] for _ in range(len(dataset)) ]
 
@@ -61,10 +102,22 @@ class Main:
 
     return input_data, label_data
 
-  def train_data(self, filename):
-    dataset = self.csv_provider.load(filename)
+  def train_data(self, path):
+    """
+    Função que vai ler os dados de um determinado caminho, obter um dataset disso, tratar esse dataset, organizando
+    os dados no formato esperado pela classe `Mlp` e vai treinar esses dados.
+
+    Parameters
+    ----------
+    path : Caminho que contém o arquivo com os dados que serão treinados
+    """
+
+    dataset = self.csv_provider.load(path)
     input_data, label_data = self.prepare_data(dataset)
     self.mlp.train(input_data, label_data)
+
+  def predict(self, path):
+    pass
 
 csvProvider = CsvProvider()
 sigmoidActivation = SigmoidActivation()
--- mlp.py
+++ mlp.py
@@ -1,7 +1,21 @@
 import numpy as np
 
 class Mlp:
+  """
+  Classe que implementa a arquitetura de uma Mlp, implementando métodos que realizam
+  as tarefas esperados por uma Rede Neural do tipo Multilayer Perceptron.
+  """
+
   def __init__(self, layers, activation_function, learning_rate=0.1, threshold=1e-3):
+    """
+    Parameters
+    ----------
+    layers : Camadas que vão compor a arquitetura da Multilayer Perceptron
+    activation_function : Classe com função de ativação e sua derivada
+    learning_rate : Taxa de aprendizado. Valor Padrão = 0.01
+    threshold : Limite de erro. Valor Padrão = 1e-3
+    """
+
     self.init_layers(layers)    
     self.activation_function = activation_function.f
     self.d_activation_function = activation_function.df
@@ -9,26 +23,60 @@ class Mlp:
     self.threshold = threshold
 
   def init_layers(self, layers):
+    """Configura as camadas de entrada, saída e escondida inicialmente. Isso envolve atribuir pesos
+    para as camadas de saída e escondida com valores aleatórios e definir as dimensões das matrizes
+    que vão compor esses pesos de acordo com os tamanhos pre-definidos de cada camada. Além disso, 
+    aqui também é definido a estrutura dos biases para as duas camadas acima, bem como são atribuídos 
+    valores aleatórios para eles.
+    
+    Parameters
+    ----------
+    layers : Camadas que vão compor a arquitetura da Multilayer Perceptron
+    """
+
     input_length = layers['input_length']
     output_length = layers['output_length']
     hidden_length = layers['hidden_length']
 
+    # Define as informações para a camada escondida
     self.hidden_layer_weights = np.random.random(input_length*hidden_length).reshape(input_length, hidden_length)
     self.hidden_layer_in = np.empty(hidden_length)
     self.hidden_layer_out = np.empty(hidden_length)
     self.hidden_layer_bias = np.random.random(hidden_length)
 
+    # Define as informações para a camada de saída
     self.output_layer_weights = np.random.random(hidden_length*output_length).reshape(hidden_length, output_length)
     self.output_layer_in = np.empty(hidden_length)
     self.output_layer_bias = np.random.random(output_length)
 
-  def feed_forward(self, data):
-    # Hidden Layer
-    self.hidden_layer_in = np.matmul(data, self.hidden_layer_weights) + self.hidden_layer_bias
+  def feed_forward(self, input_signal):
+    """Nesse método ocorre o processe de feedforward, em que ocorrerão os processos de soma de entradas ponderadas, aplicação de função de ativação para computar sinal de saída e envio para a próxima cada. Isso ocorre para a camada escondida e camada de saída.
+    
+    Parameters
+    ----------
+    input_signal : Sinal de entrada (Xi) que é dissipado para todas as unidades da camada escondida
+    """
+
+    # Para cada unidade da camada escondida(Z), soma as entradas ponderadas junto ao bias. Isso
+    # corresponde a uma multiplicação de matrizes dos sinais de entrada (Xi) pelos pesos definidos
+    # da camada escondida (Vij) e o resultado disso é somado ao bias (V0j) que resultará no Z_in
+    self.hidden_layer_in = np.matmul(input_signal, self.hidden_layer_weights) + self.hidden_layer_bias
+    
+    # Com o Z_in calculado no passo anterior, aplica-se a função de ativação para computador o sinal
+    # de saída da camada escondida (Zj). Esse sinal será encaminhado para a camada de saída para seguir
+    # com o processo.
     self.hidden_layer_out = self.activation_function(self.hidden_layer_in)
 
-    # Output Layer
+
+
+    # Para cada unidade da camada de saída (Y), soma as entradas ponderadas junto ao bias. Isso
+    # corresponde também a uma multiplicação de matrizes dos sinais de entradas vindo da camada
+    # escondida (Zj) pelos pesos definidos da camada de saída (Wjk) e o resultado disso é somado
+    # ao bias (W0k) que resultará no Y_in
     self.output_layer_in = np.matmul(self.hidden_layer_out, self.output_layer_weights) + self.output_layer_bias
+    
+    # Com o Y_in calculado anteriormente, aplica-se a função de ativação para computar o sinal de saída.
+    # Essa será a saída final
     self.output_layer_out = self.activation_function(self.output_layer_in)
 
     return self.output_layer_out
